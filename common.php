5c33c7598d948b60d2b404abdd32787a3a3a73c1
Aidan Heney <aidanheney11@gmail.com>
2016-04-29 09:36:00 -0400

    Added Base Files

---
+++ common.php
@@ -0,0 +1,51 @@
+<?php 
+
+    // These variables define the connection information for your MySQL database 
+    $username = "root"; 
+    $password = "root"; 
+    $host = "localhost"; 
+    $dbname = "testdb"; 
+     
+    // A try/catch statement is a common method of error handling in object oriented code. 
+    // First, PHP executes the code within the try block.  If at any time it encounters an 
+    // error while executing that code, it stops immediately and jumps down to the 
+    // catch block.  For more detailed information on exceptions and try/catch blocks: 
+    // http://us2.php.net/manual/en/language.exceptions.php 
+    try 
+    { 
+        // This statement opens a connection to your database using the PDO library 
+        // PDO is designed to provide a flexible interface between PHP and many 
+        // different types of database servers.  For more information on PDO: 
+        // http://us2.php.net/manual/en/class.pdo.php 
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
+    } 
+    catch(PDOException $ex) 
+    { 
+        // If an error occurs while opening a connection to your database, it will 
+        // be trapped here.  The script will output an error and stop executing. 
+        // Note: On a production website, you should not output $ex->getMessage(). 
+        // It may provide an attacker with helpful information about your code 
+        // (like your database username and password). 
+        die("Failed to connect to the database: " . $ex->getMessage()); 
+    } 
+     
+    // This statement configures PDO to throw an exception when it encounters 
+    // an error.  This allows us to use try/catch blocks to trap database errors. 
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+     
+    // This statement configures PDO to return database rows from your database using an associative 
+    // array.  This means the array will have string indexes, where the string value 
+    // represents the name of the column in your database. 
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+     
+    // This initializes a session.  Sessions are used to store information about 
+    // a visitor from one web page visit to the next.  Unlike a cookie, the information is 
+    // stored on the server-side and cannot be modified by the visitor.  However, 
+    // note that in most cases sessions do still use cookies and require the visitor 
+    // to have cookies enabled.  For more information about sessions: 
+    // http://us.php.net/manual/en/book.session.php 
+    session_start(); 
+
+    // Note that it is a good practice to NOT end your PHP files with a closing PHP tag. 
+    // This prevents trailing newlines on the file from being included in your output, 
+    // which can cause problems with redirecting users.
---
+++ edit.php
@@ -0,0 +1,106 @@
+<html>
+	<body>
+    
+	<?php
+	
+	    // pass in some info;
+		require("common.php"); 
+		
+		if(empty($_SESSION['user'])) { 
+  
+			// If they are not, we redirect them to the login page. 
+			$location = "http://" . $_SERVER['HTTP_HOST'] . "/login.php";
+			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+			//exit;
+         
+        	// Remember that this die statement is absolutely critical.  Without it, 
+        	// people can view your members-only content without logging in. 
+        	die("Redirecting to login.php"); 
+    	} 
+		
+		// To access $_SESSION['user'] values put in an array, show user his username
+		$arr = array_values($_SESSION['user']);
+		echo "Welcome " . $arr[2];
+
+		// open connection
+		$connection = mysql_connect($host, $username, $password) or die ("Unable to connect!");
+
+		// select database
+		mysql_select_db($dbname) or die ("Unable to select database!");
+
+		// create query
+		$query = "SELECT * FROM symbols";
+       
+		// execute query
+		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+
+		// see if any rows were returned
+		if (mysql_num_rows($result) > 0) {
+
+    		// print them one after another
+    		echo "<table cellpadding=10 border=1>";
+    		while($row = mysql_fetch_row($result)) {
+        		echo "<tr>";
+				echo "<td>".$row[0]."</td>";
+        		echo "<td>" . $row[1]."</td>";
+        		echo "<td>".$row[2]."</td>";
+				echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
+        		echo "</tr>";
+    		}
+		    echo "</table>";
+
+		} else {
+			
+    		// print status message
+    		echo "No rows found!";
+		}
+
+		// free result set memory
+		mysql_free_result($result);
+
+		// set variable values to HTML form inputs
+		$country = mysql_escape_string($_POST['country']);
+    	$animal = mysql_escape_string($_POST['animal']);
+		
+		// check to see if user has entered anything
+		if ($animal != "") {
+	 		// build SQL query
+			$query = "INSERT INTO symbols (country, animal) VALUES ('$country', '$animal')";
+			// run the query
+     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+			// refresh the page to show new update
+	 		echo "<meta http-equiv='refresh' content='0'>";
+		}
+		
+		// if DELETE pressed, set an id, if id is set then delete it from DB
+		if (isset($_GET['id'])) {
+
+			// create query to delete record
+			echo $_SERVER['PHP_SELF'];
+    		$query = "DELETE FROM symbols WHERE id = ".$_GET['id'];
+
+			// run the query
+     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+			
+			// reset the url to remove id $_GET variable
+			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+			exit;
+			
+		}
+		
+		// close connection
+		mysql_close($connection);
+
+	?>
+    
+    <!-- This is the HTML form that appears in the browser -->
+   	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+    	Country: <input type="text" name="country">
+    	National animal: <input type="text" name="animal">
+    	<input type="submit" name="submit">
+    </form>
+    <form action="logout.php" method="post"><button>Log out</button></form>
+    
+	</body>
+</html>
