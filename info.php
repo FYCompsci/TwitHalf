bc7c11752d47d58e41e5f3760cb7ecfe93107821
malsf21 <matthewang.wang@gmail.com>
2016-05-05 11:32:58 -0400

    changes info code

--- info.php
+++ info.php
@@ -7,21 +7,31 @@
     die("Redirecting to home.php");
   }
   require("common.php");
-  function pull_posts(){
-		$query = "
-     SELECT * FROM thebuzz.posts WHERE username='"+ $page_user +"'
-    ";
+  $query = "
+      SELECT
+          *
+      FROM info
+      WHERE
+          username = :username
+  ";
+  $query_params = array(
+      ':username' => $page_user
+  );
 
-    $result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+  try
+  {
+      $stmt = $db->prepare($query);
+      $result = $stmt->execute($query_params);
+  }
+  catch(PDOException $ex)
+  {
+      die("Failed to run query: " . $ex->getMessage());
+  }
 
-		$data = [];
-		while($row = mysql_fetch_row($result)) {
-			array_push($data, $row);
-		}
+  $row = $stmt->fetch();
 
-		return(json_encode($data));
-		mysql_free_result($result);
-		mysql_close($connection);
-	}
-	echo pull_posts();
+  if($row)
+  {
+    echo json_encode($row);
+  }
 ?>
