7b34217640e21e2a6bfe98eaf9b4b5d2543148df
Always98 <saeed.khan@saeedkhan.local>
2016-05-09 13:46:04 -0400

    made team.php (buzz team member's page)

---
+++ team.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="author" content="">
+		<title>About | The Buzz</title>
+		<link href="favicon.ico" rel="icon" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<?php include_once ('navbar.php'); ?>
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">Matthew Wang <span class="text-muted">@malsf21</span></h4>
+              <p class="card-text">The main coder and brains behind The Buzz, Matthew Wang's eager to help you with any of your problems!</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <blockquote class="blockquote">
+            Thanks for using The Buzz, be sure to <i>buzz</i> on about how great we are!
+          </blockquote>
+        </div>
+      </div>
+      <div class="row">
+        
+        <div class="col-sm-8">
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">Aidan Heney <span class="text-muted">@a11heney</span></h4>
+              <p class="card-text">The Github co-ordinator who keeps you up to date on any new improvements to the site!</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">Saeed Khan<span class="text-muted"> @Always98</span></h4>
+              <p class="card-text">Nothing special, but he made this page (with the help of Matthew Wang and Bootstrap).</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include_once ('footer.php'); ?>
+		<script src="js/jquery2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
