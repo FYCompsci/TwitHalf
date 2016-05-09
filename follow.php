e663802b7d8e15020bd0a73603337a07b6b9454a
malsf21 <matthewang.wang@gmail.com>
2016-05-09 13:36:53 -0400

    fixes logic issue where wrong column is updated

--- follow.php
+++ follow.php
@@ -24,15 +24,50 @@
     if($row)
     {
         $following = $row['following'];
-        $followers = $row['followers'] + 1;
     }
     $query = "
-      UPDATE info SET following=:following,followers=:followers WHERE username=:username
+      UPDATE info SET following=:following WHERE username=:username
     ";
     $query_params = array(
       ':following' => $following.",".$_GET['follow'],
-      ':followers' => $followers,
       ':username' => $_SESSION['user']['username']
+    );
+    try
+    {
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex)
+    {
+        die("Failed to run query: " . $ex->getMessage());
+    }
+
+    $query_params = array(
+        ':username' => $_GET['follow']
+    );
+
+    try
+    {
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex)
+    {
+        die("Failed to run query: " . $ex->getMessage());
+    }
+
+    $row = $stmt->fetch();
+
+    if($row)
+    {
+        $followers = $row['followers'] + 1;
+    }
+    $query = "
+      UPDATE info SET followers=:followers WHERE username=:username
+    ";
+    $query_params = array(
+      ':followers' => $followers,
+      ':username' => $_GET['follow']
     );
     try
     {
@@ -71,18 +106,53 @@
     if($row)
     {
         $following = $row['following'];
-        $followers = $row['followers'] -1;
     }
     $comma_string = ",".$_GET['unfollow'];
     $following = str_replace($comma_string, '', $following);
     $following = str_replace($_GET['unfollow'], '', $following);
     $query = "
-      UPDATE info SET following=:following,followers=:followers WHERE username=:username
+      UPDATE info SET following=:following WHERE username=:username
     ";
     $query_params = array(
       ':following' => $following,
-      ':followers' => $followers,
       ':username' => $_SESSION['user']['username']
+    );
+    try
+    {
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex)
+    {
+        die("Failed to run query: " . $ex->getMessage());
+    }
+
+    $query_params = array(
+        ':username' => $_GET['unfollow']
+    );
+
+    try
+    {
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex)
+    {
+        die("Failed to run query: " . $ex->getMessage());
+    }
+
+    $row = $stmt->fetch();
+
+    if($row)
+    {
+        $followers = $row['followers'] - 1;
+    }
+    $query = "
+      UPDATE info SET followers=:followers WHERE username=:username
+    ";
+    $query_params = array(
+      ':followers' => $followers,
+      ':username' => $_GET['unfollow']
     );
     try
     {
