aed8d382bd85aa6fcd93ebeae891be30658e32d1
malsf21 <matthewang.wang@gmail.com>
2016-05-09 13:56:35 -0400

    formats out team cards

--- team.php
+++ team.php
@@ -18,32 +18,66 @@
         <div class="col-sm-8">
           <div class="card">
             <div class="card-block">
-              <h4 class="card-title">Matthew Wang <span class="text-muted">@malsf21</span></h4>
-              <p class="card-text">The main coder and brains behind The Buzz, Matthew Wang's eager to help you with any of your problems!</p>
+							<div class="row">
+								<div class="col-sm-2">
+									<img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/malsf21.png" alt="The drones bees are almost done their work!">
+								</div>
+								<div class="col-sm-10">
+		              <h4 class="card-title">Matthew Wang <span class="text-muted">@malsf21</span></h4>
+		              <p class="card-text">The main coder and brains behind The Buzz, Matthew Wang's eager to help you with any of your problems!</p>
+								</div>
+							</div>
             </div>
           </div>
         </div>
         <div class="col-sm-4">
           <blockquote class="blockquote">
-            Thanks for using The Buzz, be sure to <i>buzz</i> on about how great we are!
+            The Buzz is a work of <span class="fa fa-heart" style="color:red;"></span> love <span class="fa fa-heart" style="color:red;"></span> made by The Buzz team. We work hard to make sure that The Buzz becomes the best social network it can <b>bee</b>, and to make sure that everything is in tip top shape!
           </blockquote>
         </div>
       </div>
       <div class="row">
-        
+				<div class="col-sm-4">
+					<blockquote class="blockquote blockquote-reverse">
+          	The Buzz is made in PHP, HTML, CSS, and JavaScript, and relies on a few awesome libraries (namely Bootstrap and Font-Awesome). Without those frameworks, we wouldn't be here today!
+          </blockquote>
+				</div>
         <div class="col-sm-8">
           <div class="card">
             <div class="card-block">
-              <h4 class="card-title">Aidan Heney <span class="text-muted">@a11heney</span></h4>
-              <p class="card-text">The Github co-ordinator who keeps you up to date on any new improvements to the site!</p>
+							<div class="row">
+								<div class="col-sm-2">
+									<img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/a11heney.png" alt="The drones bees are almost done their work!">
+								</div>
+								<div class="col-sm-10">
+		              <h4 class="card-title">Aidan Heney <span class="text-muted">@a11heney</span></h4>
+		              <p class="card-text">The Github co-ordinator who keeps you up to date on any new improvements to the site!</p>
+								</div>
+							</div>
             </div>
           </div>
+				</div>
+			</div>
+			<div class="row">
+        <div class="col-sm-8">
           <div class="card">
             <div class="card-block">
-              <h4 class="card-title">Saeed Khan<span class="text-muted"> @Always98</span></h4>
-              <p class="card-text">Nothing special, but he made this page (with the help of Matthew Wang and Bootstrap).</p>
+							<div class="row">
+								<div class="col-sm-2">
+									<img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/Always98.png" alt="The drones bees are almost done their work!">
+								</div>
+								<div class="col-sm-10">
+		              <h4 class="card-title">Saeed Khan<span class="text-muted"> @Always98</span></h4>
+		              <p class="card-text">Nothing special, but he made this page (with the help of Matthew Wang and Bootstrap).</p>
+								</div>
+							</div>
             </div>
           </div>
+        </div>
+				<div class="col-sm-4">
+          <blockquote class="blockquote">
+            Thanks for using The Buzz! You can <i>buzz<i> about our team with the hashtag <a href="hashtag.php?hashtag=#TheBuzz">#TheBuzz</a>!
+          </blockquote>
         </div>
       </div>
     </div>
