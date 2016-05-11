bbaa8b07e2e86457ac89ea4be4f17c3c22ec9ae1
malsf21 <matthewang.wang@gmail.com>
2016-05-11 10:14:01 -0400

    adds a little debugging

--- js/buildUsers.js
+++ js/buildUsers.js
@@ -31,50 +31,59 @@ function buildUsers(username,method){
   for (var key in infoData){
     if (infoData[key]["username"] == username || method != "single"){
       arr.push(infoData[key]);
+      console.log("Match found!");
     }
   }
 
-  if (method == "followers"){
-    arr.sort(compareFollowers);
+  if (arr.length === 0){
+    $("feed-container").html("This honeycomb is empty. Are you sure you searched for the right bee?")
   }
-  for (var i=0; i<arr.length; i++) {
-    if (arr[i][1] != page_username){
-      if ($.inArray(arr[i][1], userInfoData['following'].split(",")) > -1 ){
-        fbutton = "<a class='btn btn-block btn-info-outline' href='follow.php?unfollow="+arr[i][1]+"'><span class='fa fa-check'></span> Following " + arr[i][1] + "</a>";
+  else{
+
+    if (method == "followers"){
+      arr.sort(compareFollowers);
+    }
+    for (var i=0; i<arr.length; i++) {
+      console.log("Rendering!");
+      if (arr[i][1] != page_username){
+        if ($.inArray(arr[i][1], userInfoData['following'].split(",")) > -1 ){
+          fbutton = "<a class='btn btn-block btn-info-outline' href='follow.php?unfollow="+arr[i][1]+"'><span class='fa fa-check'></span> Following " + arr[i][1] + "</a>";
+        }
+        else{
+          fbutton = "<a class='btn btn-block btn-info-outline' href='follow.php?follow="+arr[i][1]+"'><span class='fa fa-plus'></span> Follow " + arr[i][1] + "</a>";
+        }
       }
       else{
-        fbutton = "<a class='btn btn-block btn-info-outline' href='follow.php?follow="+arr[i][1]+"'><span class='fa fa-plus'></span> Follow " + arr[i][1] + "</a>";
+        fbutton = "<a class='btn btn-block btn-info-outline' href='user.php?username="+page_username+"'>Queen Bee</a>";
       }
-    }
-    else{
-      fbutton = "<a class='btn btn-block btn-info-outline' href='user.php?username="+page_username+"'>Queen Bee</a>";
-    }
-    following = arr[i][3].split(",").length;
-    followers = arr[i][4];
-    $("#feed-container").append('<div class="card"><div class="card-block"><div class="row"><div class="col-sm-2"><img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/'+arr[i][1]+'.png" alt="The drones bees are almost done their work!"></div><div class="col-sm-10"><div class="row"><div class="col-sm-9"><h4 class="card-title">@'+arr[i][1]+'</h4><h6>' + following + ' Following, ' + followers + ' Followers</div><div class="col-sm-3">' + fbutton + '</div></div></div></div></div></div>');
-    /*
-    Here's the non-minified version of the template of each user. Unfortunately, JS variables don't support newlines, so we need to condense it before it is appended to the container.
-    <div class="card">
-      <div class="card-block">
-        <div class="row">
-          <div class="col-sm-2">
-            <img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/'+arr[i][1]+'.png" alt="The drones bees are almost done their work!">
-          </div>
-          <div class="col-sm-10">
-            <div class="row">
-              <div class="col-sm-9">
-                <h4 class="card-title">@'+arr[i][1]+'</h4>
-                <h6>' + following + ' Following, ' + followers + ' Followers
-              </div>
-              <div class="col-sm-3">
-                ' + fbutton + '
+      following = arr[i][3].split(",").length;
+      followers = arr[i][4];
+      console.log("Printing!");
+      $("#feed-container").append('<div class="card"><div class="card-block"><div class="row"><div class="col-sm-2"><img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/'+arr[i][1]+'.png" alt="The drones bees are almost done their work!"></div><div class="col-sm-10"><div class="row"><div class="col-sm-9"><h4 class="card-title">@'+arr[i][1]+'</h4><h6>' + following + ' Following, ' + followers + ' Followers</div><div class="col-sm-3">' + fbutton + '</div></div></div></div></div></div>');
+      /*
+      Here's the non-minified version of the template of each user. Unfortunately, JS variables don't support newlines, so we need to condense it before it is appended to the container.
+      <div class="card">
+        <div class="card-block">
+          <div class="row">
+            <div class="col-sm-2">
+              <img class="img-fluid img-thumbnail center-block" src="https://api.adorable.io/avatars/64/'+arr[i][1]+'.png" alt="The drones bees are almost done their work!">
+            </div>
+            <div class="col-sm-10">
+              <div class="row">
+                <div class="col-sm-9">
+                  <h4 class="card-title">@'+arr[i][1]+'</h4>
+                  <h6>' + following + ' Following, ' + followers + ' Followers
+                </div>
+                <div class="col-sm-3">
+                  ' + fbutton + '
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    */
-
+      */
+      console.log("Finished!");
+    }
   }
 }
