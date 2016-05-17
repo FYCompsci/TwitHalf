d487a6ce36bf78e8b161fd796e5a787f1da2df54
Aidan Heney <aidan.heney@ucc.on.ca>
2016-05-17 10:54:57 -0400

    Fixing grammar for about page

--- about.php
+++ about.php
@@ -19,14 +19,14 @@
 				<div class="col-sm-4">
 				<h2>Buzz</h2>
 				<br>
-					<p>To like a post, just click the heart icon! All your own posts are automatically liked by default.
+					<p>To like a post, just click the heart icon underneath it! All your own posts are liked by default.
 						 If you want to unlike a post, simply click on the heart icon again.</p>
 						<br>
 							<p>Show fellow buzzers some love by re-buzzing their posts! Just click the arrow square underneath
 								 their post and it will show up on everyone's feeds!
 						</br>
 						</br>
-						<p>Don't like one of your posts? Just click the 'x' underneath the buzz you want to delete and its gone!
+						<p>Don't like one of your posts? Just click the 'x' underneath the post you want to delete and it's gone!
 							 Just remember, if you delete it then it's gone forever!</p>
 				</br>
 				</div>
@@ -41,9 +41,9 @@
 							 Once you follow them, their buzzes will show up on your feed!</p>
 						</br>
 							<p>Want to mention someone in one of your posts? Just type '@', and then their name (no spaces)!
-									This will automatically tag them yo your post, and it will show up in their feed!</p>
+									This will tag them to your post and it will show up in their feed!</p>
 						</br>
-						<p>If you see a a problem with someone's post, click the hazard symbol just underneath it and you will be
+						<p>If you see a problem with someone's post, click the hazard symbol just underneath it and you will be
 								 directed to our report page. We will then attempt to solve the issue at hand. Remember to bee friendly
 								 on The Buzz!</p>
 				</br>
@@ -54,13 +54,13 @@
 				<div class="col-sm-4">
 				<h2>Discover</h2>
 				<br>
-					<p>Do you want to see the most liked buzzes of all time? Simply click on the 'Discover' tab on the navigation
-						bar and then click 'top' in the dropdown.</p>
+					<p>Do you want to see the most liked buzzes of all time? Simply click the 'Discover' tab on the navigation
+						bar and then click 'top' in the dropdown menu.</p>
 						</br>
 							<p>To see the newest buzzes from people you aren't following, simply click 'new' in the discover dropdown
 								menu. Maybe you will find some new favourite buzzers!</p>
 						</br>
-						<p>If you want to search for a specific buzzer, or a specific hashtag, simply click search on the discover
+						<p>If you want to search for a specific buzzer or specific hashtag, simply click search on the discover
 							dropdown menu! You will be directed to a new page where it is easy to find what you're looking for!</p>
 						</br>
 						</div>
