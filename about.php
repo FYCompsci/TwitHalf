19a0e9ff40a77c96e89f82541543350969cc4d23
Aidan Heney <aidan.heney@ucc.on.ca>
2016-05-17 10:22:29 -0400

    Finished about page

--- about.php
+++ about.php
@@ -14,45 +14,61 @@
 	</head>
 	<body>
 		<?php include_once ('navbar.php'); ?>
-      <div class="container">
-      <div class="row">
-        <div class="col-sm-8">
-          <div class="card">
-            <div class="card-block">
-              <h4 class="card-title">Matthew Wang <span class="text-muted">@malsf21</span></h4>
-              <p class="card-text">@hedgeriot I'm loving the functionality of @GitHub and @Atom combined together, you should try it #coding #compsci #webdesign</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <blockquote class="blockquote">
-            Talk about things to people simply and quickly
-          </blockquote>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <blockquote class="blockquote blockquote-reverse">
-            Connect with other coders around the world
-          </blockquote>
-        </div>
-        <div class="col-sm-8">
-          <div class="card">
-            <div class="card-block">
-              <h4 class="card-title">John Mace <span class="text-muted">@hedgeriot</span></h4>
-              <p class="card-text">Anybody seen the new @Bootstrap toolkit? #awesome</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text text-muted"><span class="fa fa-reply"></span> in response to @hedgeriot</p>
-              <h4 class="card-title">Matthew Wang <span class="text-muted">@malsf21</span></h4>
-              <p class="card-text">Yeah man, it looks amazing! Props to @Bootstrap ! I'll probably use it in some of my #webdesign projects soon!</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+				<h2>Buzz</h2>
+				<br>
+					<p>To like a post, just click the heart icon! All your own posts are automatically liked by default.
+						 If you want to unlike a post, simply click on the heart icon again.</p>
+						<br>
+							<p>Show fellow buzzers some love by re-buzzing their posts! Just click the arrow square underneath
+								 their post and it will show up on everyone's feeds!
+						</br>
+						</br>
+						<p>Don't like one of your posts? Just click the 'x' underneath the buzz you want to delete and its gone!
+							 Just remember, if you delete it then it's gone forever!</p>
+				</br>
+				</div>
+
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+				<h2>Connect</h2>
+				<br>
+					<p>If you want to see what your friends are up to, click on their username and press follow.
+							 Once you follow them, their buzzes will show up on your feed!</p>
+						</br>
+							<p>Want to mention someone in one of your posts? Just type '@', and then their name (no spaces)!
+									This will automatically tag them yo your post, and it will show up in their feed!</p>
+						</br>
+						<p>If you see a a problem with someone's post, click the hazard symbol just underneath it and you will be
+								 directed to our report page. We will then attempt to solve the issue at hand. Remember to bee friendly
+								 on The Buzz!</p>
+				</br>
+				</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+				<h2>Discover</h2>
+				<br>
+					<p>Do you want to see the most liked buzzes of all time? Simply click on the 'Discover' tab on the navigation
+						bar and then click 'top' in the dropdown.</p>
+						</br>
+							<p>To see the newest buzzes from people you aren't following, simply click 'new' in the discover dropdown
+								menu. Maybe you will find some new favourite buzzers!</p>
+						</br>
+						<p>If you want to search for a specific buzzer, or a specific hashtag, simply click search on the discover
+							dropdown menu! You will be directed to a new page where it is easy to find what you're looking for!</p>
+				</br>
+				</div>
+				</div>
+				</div>
+			</div>
+		</div>
+
     <?php include_once ('footer.php'); ?>
 		<script src="js/jquery2.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
